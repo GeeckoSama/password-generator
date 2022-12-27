50a77c6e13febd1763a0b08f7cb749a0b1770cbe
GeeckoSama <julienheller81@gmail.com>
2022-12-27 23:50:21 +0100

    First commit with a basic component & test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,86 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+describe('AppComponent (avec spectator)', () => {
+  let spectator: Spectator<AppComponent>;
+  let component: AppComponent;
+
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    declarations: [AppComponent],
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
   });
 
-  it(`should have as title 'password-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('password-generator');
+  it('should work', () => {
+    expect(spectator.query('article')?.textContent).toBe(
+      'Cliquez sur le bouton Générer'
+    );
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('password-generator app is running!');
+  it('should change message when user clicks generate button', () => {
+    spectator.click('button');
+    expect(spectator.query('article')).toHaveText('MON_MOT_DE_PASSE');
+  });
+
+  it('should update settings when user clicks on checkboxes', () => {
+    spectator.click('#uppercase');
+    expect(component.uppercase).toBeTrue();
+    
+
+    spectator.click('#numbers');
+    expect(component.numbers).toBeTrue();
+
+    spectator.click('#symbols');
+    expect(component.symbols).toBeTrue();
+
+    spectator.typeInElement("33", '#length');
+    expect(component.length).toBe(33);
+  });
+});
+
+describe('AppComponent (avec TestBed)', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.autoDetectChanges();
+
+    component = fixture.componentInstance;
+  });
+  it('should work', () => {
+    const article = fixture.nativeElement.querySelector('article');
+    expect(article.textContent).toBe('Cliquez sur le bouton Générer');
+  });
+
+  it('should change message when user clicks generate button', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+
+    const article = fixture.nativeElement.querySelector('article');
+    expect(article.textContent).toBe('MON_MOT_DE_PASSE');
+  });
+
+  it('should update settings when user clicks on checkboxes', () => {
+    fixture.nativeElement.querySelector('#uppercase').click();
+    expect(component.uppercase).toBeTrue();
+
+    fixture.nativeElement.querySelector('#numbers').click();
+    expect(component.numbers).toBeTrue();
+
+    fixture.nativeElement.querySelector('#symbols').click();
+    expect(component.symbols).toBeTrue();
+
+    const length = fixture.nativeElement.querySelector('#length');
+    length.value = 33;
+    length.dispatchEvent(new Event('input'));
+    expect(component.length).toBe(33);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,103 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>Générer un mot de passe fort !</h1>
+      <div class="grid">
+        <div>
+          <h3>Votre futur mot de passe</h3>
+          <article>{{ message }}</article>
+        </div>
+        <div>
+          <label for="length">Longueur du mot de passe : {{ length }}</label>
+          <input
+            type="range"
+            name="length"
+            id="length"
+            min="10"
+            max="50"
+            (input)="onChangeLength($event)"
+          />
+
+          <label for="uppercase">
+            <input
+              #uppercaseInput
+              (change)="
+                onChangeSetting(uppercaseInput.name, uppercaseInput.checked)
+              "
+              role="switch"
+              type="checkbox"
+              name="uppercase"
+              id="uppercase"
+            />
+            Contiendra des majuscules
+          </label>
+          <label for="numbers">
+            <input
+              #numbersInput
+              (change)="
+                onChangeSetting(numbersInput.name, numbersInput.checked)
+              "
+              role="switch"
+              type="checkbox"
+              name="numbers"
+              id="numbers"
+            />
+            Contiendra des nombres
+          </label>
+          <label for="symbols">
+            <input
+              #symbolsInput
+              (change)="
+                onChangeSetting(symbolsInput.name, symbolsInput.checked)
+              "
+              role="switch"
+              type="checkbox"
+              name="symbols"
+              id="symbols"
+            />
+            Contiendra des caractères spéciaux
+          </label>
+          <hr />
+          <button (click)="onClickGenerate()">Générer</button>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [],
 })
 export class AppComponent {
-  title = 'password-generator';
+  message = 'Cliquez sur le bouton Générer';
+
+  length = 20;
+  uppercase = false;
+  numbers = false;
+  symbols = false;
+
+  onClickGenerate() {
+    this.message = 'MON_MOT_DE_PASSE';
+    console.log('Génération du mot de passe avec');
+    console.table({
+      Length: this.length,
+      Uppercase: this.uppercase,
+      Numbers: this.numbers,
+      Symbols: this.symbols,
+    });
+  }
+
+  onChangeLength(event: Event) {
+    this.length = +(event.target as HTMLInputElement).value;
+  }
+
+  onChangeSetting(settingName: string, settingValue: boolean) {
+    if (
+      settingName !== 'uppercase' &&
+      settingName !== 'numbers' &&
+      settingName !== 'symbols'
+    )
+      return;
+
+    this[settingName] = settingValue;
+  }
 }
